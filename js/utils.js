d84c2e11a95d1fd2578493d1abbbd871b053024f
Devang <devngc@gmail.com>
2022-09-12 17:44:26 -0400

    fix(*): finish app

---
+++ js/utils.js
@@ -0,0 +1,25 @@
+import characters from "./characters.js";
+
+function update() {
+    let pass = '';
+    for (let i = 0; i < 15; i++){
+        const num = Math.floor(Math.random() * 91);
+        pass += characters[num];
+    }
+    return pass;
+}
+
+
+
+function copy(pass) {
+    if (pass) {
+        navigator.clipboard.writeText(pass);
+        alert("Copied the text: " + pass);
+    } else {
+        alert("Generate passwords first");
+    }
+}
+
+export { update, copy };
+
+
